b8052f6e364d9b04621fd7f2bae50e1d7a4d2357
StevsCode <stevensavarese1@gmail.com>
2025-04-03 21:19:13 +0200

    Add a New Book button that brings up a form allowing users to input the details for the new book and add it to the library: author, title, year, whether it’s been read

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const myLibrary = [];
 
-function Book(title, author, genre, year) {
+function Book(title, author, genre, year, read) {
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.genre = genre;
   this.year = year;
+  this.read = read;
 }
 
-function addBookToLibrary(title, author, genre, year) {
-  const newBook = new Book(title, author, genre, year);
+function addBookToLibrary(title, author, genre, year, read) {
+  const newBook = new Book(title, author, genre, year, read);
   myLibrary.push(newBook);
 }
 
@@ -26,9 +27,34 @@ function displayBooks() {
       <p><strong>Author:</strong> ${book.author}</p>
       <p><strong>Genre:</strong> ${book.genre}</p>
       <p><strong>Year:</strong> ${book.year}</p>
-      <p><strong>ID:</strong> ${book.id}</p>
+      <p><strong>Read:</strong> ${book.read}</p>
     `;
 
     libraryDisplay.appendChild(bookCard);
   });
 }
+
+document.getElementById("new-book-btn").addEventListener("click", () => {
+  document.getElementById("book-form-modal").showModal();
+});
+
+document.getElementById("close-modal").addEventListener("click", () => {
+  document.getElementById("book-form-modal").closest();
+});
+document.getElementById("book-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const genre = document.getElementById("genre").value;
+  const year = document.getElementById("year").value;
+  const read = document.getElementById("read").checked ? "Yes" : "No";
+
+  addBookToLibrary(title, author, genre, year);
+
+  displayBooks();
+
+  document.getElementById("book-form").reset();
+
+  document.getElementById("book-form-modal").close();
+});
